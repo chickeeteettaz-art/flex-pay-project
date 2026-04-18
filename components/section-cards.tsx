8a42ca90c945fb6356c283a39ad499ab794488e6
Chicketeetta Zinhle <chickeeteettaz@gmail.com>
2026-04-18 20:09:46 +0200

    Account and payment card

--- components/section-cards.tsx
+++ components/section-cards.tsx
@@ -9,7 +9,7 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
-import {TrendingUpIcon, TrendingDownIcon, HandCoins} from "lucide-react"
+import {TrendingUpIcon, TrendingDownIcon, HandCoins, CreditCard} from "lucide-react"
 
 export function SectionCards() {
   return (
@@ -18,7 +18,7 @@ export function SectionCards() {
         <CardHeader>
           <CardDescription>Account Balance</CardDescription>
           <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
-            R17,250.0
+            R17,250.00
           </CardTitle>
           <CardAction>
             <Badge variant="outline" className='px-4 py-1.5'>
@@ -30,35 +30,36 @@ export function SectionCards() {
         </CardHeader>
         <CardFooter className="flex-col items-start gap-1.5 text-sm">
           <div className="line-clamp-1 flex gap-2 font-medium">
-            Trending up this month{" "}
-            <TrendingUpIcon className="size-4" />
+              <CreditCard className="size-4"/>
+            4473 2190 893
+
           </div>
           <div className="text-muted-foreground">
-            Visitors for the last 6 months
+            Current Account Balance
           </div>
         </CardFooter>
       </Card>
       <Card className="@container/card">
         <CardHeader>
-          <CardDescription>New Customers</CardDescription>
+          <CardDescription>Total Payments</CardDescription>
           <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
-            1,234
+             - R9 600.00
           </CardTitle>
           <CardAction>
             <Badge variant="outline">
-              <TrendingDownIcon
+              <TrendingUpIcon
               />
-              -20%
+
             </Badge>
           </CardAction>
         </CardHeader>
         <CardFooter className="flex-col items-start gap-1.5 text-sm">
           <div className="line-clamp-1 flex gap-2 font-medium">
-            Down 20% this period{" "}
-            <TrendingDownIcon className="size-4" />
+            Account payments
+            <TrendingUpIcon className="size-4" />
           </div>
           <div className="text-muted-foreground">
-            Acquisition needs attention
+            Total amount of payments made
           </div>
         </CardFooter>
       </Card>
