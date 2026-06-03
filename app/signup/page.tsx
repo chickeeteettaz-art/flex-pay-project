cabd31d097e19cd4cbbb310ded467623d9f65184
Chickeeteetta <chickeeteettaz@gmail.com>
2026-06-03 22:07:04 +0200

    Code smell update

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -4,7 +4,7 @@
 
 import Link from 'next/link'
 import { Button } from '@/components/ui/button'
-import { ArrowLeft, Home } from 'lucide-react'
+import { Home } from 'lucide-react'
 
 export default function SignupPage() {
   return (
